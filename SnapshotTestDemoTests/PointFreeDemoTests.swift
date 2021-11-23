9422074a473bf5f4e312c2790e174dfad6b6e99b
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-11-23 07:14:00 -0500

    Update overview

--- SnapshotTestDemoTests/PointFreeDemoTests.swift
+++ SnapshotTestDemoTests/PointFreeDemoTests.swift
@@ -24,7 +24,13 @@ class PointFreeDemoTests: XCTestCase {
 
   func test_ShowWithLargeName_PointFree() {
     // given
-    let model = TVShowDetailInfo.stub(name: "This a TVShow with a large Name, that doesnt fit in the screen")
+    let overview = "Fringe is an American science fiction television series created by J. J. ... " +
+      "The team uses fringe science and FBI investigative techniques to investigate" +
+      "a series of unexplained, often ghastly occurrences, which are related to " +
+      "mysteries surrounding a parallel universe."
+
+    let model = TVShowDetailInfo.stub(name: "This a TVShow with a large Name, that doesnt fit in the screen",
+                                      overView: overview)
     let viewController = TVShowDetailViewController(model: model)
 
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
