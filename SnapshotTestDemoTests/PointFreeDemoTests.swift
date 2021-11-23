fceaa87ffe7c75c8efcd164e29a8145d4e76c106
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-11-23 06:59:41 -0500

    Update snapshots for pointfree suite tests

--- SnapshotTestDemoTests/PointFreeDemoTests.swift
+++ SnapshotTestDemoTests/PointFreeDemoTests.swift
@@ -12,30 +12,22 @@ import SnapshotTesting
 @testable import SnapshotTestDemo
 
 class PointFreeDemoTests: XCTestCase {
-
+  
   func test_BreakingBad_PointFree() {
     // given
     let model = TVShowDetailInfo.stub(name: "Breaking Bad")
     let viewController = TVShowDetailViewController(model: model)
-
+    
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
-    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneX)))
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
-
-    // rootWindow.rootViewController = viewController
   }
-
+  
   func test_ShowWithLargeName_PointFree() {
     // given
     let model = TVShowDetailInfo.stub(name: "This a TVShow with a large Name, that doesnt fit in the screen")
     let viewController = TVShowDetailViewController(model: model)
-
-    assertSnapshot(matching: viewController, as: .image)
-
+    
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
-    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneX)))
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
-
-    // rootWindow.rootViewController = viewController
   }
 }
