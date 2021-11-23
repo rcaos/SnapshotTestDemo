2dfe3c02c16d6d08fba223372fcc06c8fb64511b
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-11-23 07:10:28 -0500

    Fix identation

--- SnapshotTestDemoTests/PointFreeDemoTests.swift
+++ SnapshotTestDemoTests/PointFreeDemoTests.swift
@@ -12,21 +12,21 @@ import SnapshotTesting
 @testable import SnapshotTestDemo
 
 class PointFreeDemoTests: XCTestCase {
-  
+
   func test_BreakingBad_PointFree() {
     // given
     let model = TVShowDetailInfo.stub(name: "Breaking Bad")
     let viewController = TVShowDetailViewController(model: model)
-    
+
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
   }
-  
+
   func test_ShowWithLargeName_PointFree() {
     // given
     let model = TVShowDetailInfo.stub(name: "This a TVShow with a large Name, that doesnt fit in the screen")
     let viewController = TVShowDetailViewController(model: model)
-    
+
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
   }
