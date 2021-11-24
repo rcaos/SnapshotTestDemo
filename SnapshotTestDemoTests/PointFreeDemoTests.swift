a870c7158ef7770a8cfa6e86a0301b174ecb8191
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-11-24 07:32:45 -0500

    Update tests files and names

--- SnapshotTestDemoTests/PointFreeDemoTests.swift
+++ SnapshotTestDemoTests/PointFreeDemoTests.swift
@@ -17,27 +17,27 @@ class PointFreeDemoTests: XCTestCase {
     isRecording = false
   }
 
-  func test_BreakingBad_PointFree() {
+  func test_TheWalkingDead_PointFree() {
     // given
-    let model = TVShowDetailInfo.stub(name: "Breaking Bad")
+    let model = TVShowDetailInfo.stub(name: "The Walking Dead")
     let viewController = TVShowDetailViewController(model: model)
 
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
-    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
+    // assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
   }
 
-  func test_ShowWithLargeName_PointFree() {
+  func test_TVShowWithLargeName_PointFree() {
     // given
     let overview = "Fringe is an American science fiction television series created by J. J. ... " +
       "The team uses fringe science and FBI investigative techniques to investigate" +
       "a series of unexplained, often ghastly occurrences, which are related to " +
       "mysteries surrounding a parallel universe."
 
-    let model = TVShowDetailInfo.stub(name: "This a TVShow with a large Name, that doesnt fit in the screen",
+    let model = TVShowDetailInfo.stub(name: "This a TVShow with a large Name, that does not fit in the screen",
                                       overView: overview)
     let viewController = TVShowDetailViewController(model: model)
 
     assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneSe)))
-    assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
+    // assertSnapshot(matching: viewController, as: .wait(for: 1, on: .image(on: .iPhoneXsMax)))
   }
 }
